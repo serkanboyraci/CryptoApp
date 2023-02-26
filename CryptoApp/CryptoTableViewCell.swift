c5daded8167927e1f5ac901186c57187aa4516d7
serkanboyraci <serkanboyraci@gmail.com>
2023-02-26 13:51:05 +0300

    tableView added

---
+++ CryptoApp/CryptoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CryptoTableViewCell.swift
+//  CryptoApp
+//
+//  Created by Ali serkan Boyracı  on 25.02.2023.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CryptoApp/ViewController.swift
+++ CryptoApp/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
       
         
@@ -19,5 +24,21 @@ class ViewController: UIViewController {
     
 
 
+}
+
+extension ViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+extension ViewController : UITableViewDelegate {
+    
 }
 
