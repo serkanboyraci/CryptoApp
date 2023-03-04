062f2257a445b9a1625f61c54bda3fdf4e517eb7
serkanboyraci <serkanboyraci@gmail.com>
2023-03-04 13:36:04 +0300

    Update WebService.swift

--- CryptoApp/Service/WebService.swift
+++ CryptoApp/Service/WebService.swift
@@ -7,8 +7,22 @@
 
 import Foundation
 
-class WebService {
-    func downloadCurrencies() {
-        
+class Webservice {
+    func downloadCurrencies(url: URL, completion: @escaping ([CryptoElement]?) -> ()) { // escaping = after completion loading and use this func.
+        // cryptoelemant is array beacuse of this we must use square brackets. Also we use it as a optional
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil) // if there is error no data will be taken
+            } else if let data = data {
+                let cryptolist = try? JSONDecoder().decode([CryptoElement].self, from: data)
+                
+                print(cryptolist)
+                
+                if let cryptolist = cryptolist {
+                    completion(cryptolist)
+                }
+            }
+        }.resume() // to start work
     }
 }
